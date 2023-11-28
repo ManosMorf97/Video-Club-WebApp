4cc0e5e23f68fe6f9a4233d3d4b52cf46041f36d
ManosMorf97 <manosmorf97@gmail.com>
2023-11-29 00:57:32 +0200

    Update

--- movies/src/main/java/com/example/movies/controller/SignUpController.java
+++ movies/src/main/java/com/example/movies/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.example.movies.controller;
 
+import com.example.movies.dao.DAO;
 import com.example.movies.dao.UsersJdbcDAO;
 import com.example.movies.model.User;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SignUpController {
 
-    private UsersJdbcDAO usersJdbcDAO;
+    private DAO<User> dao;
 
-    public SignUpController(UsersJdbcDAO usersJdbcDAO){
-        this.usersJdbcDAO=usersJdbcDAO;
+    public SignUpController(DAO<User> dao){
+        this.dao=dao;
     }
 
     @PostMapping("localhost:8080/SignUp")
     public void insertNewUser(User user){
-        usersJdbcDAO.Create(user);
+        dao.Create(user);
     }
 }
