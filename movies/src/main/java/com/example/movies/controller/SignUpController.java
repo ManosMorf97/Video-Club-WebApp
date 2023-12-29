5d45d6766f2884af70f885a28b7aa552293f5e61
ManosMorf97 <manosmorf97@gmail.com>
2023-12-29 15:50:58 +0200

    Better

--- movies/src/main/java/com/example/movies/controller/SignUpController.java
+++ movies/src/main/java/com/example/movies/controller/SignUpController.java
@@ -4,7 +4,9 @@ import com.example.movies.dao.DAO;
 import com.example.movies.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +18,9 @@ public class SignUpController {
         this.dao=dao;
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
     @PostMapping("/SignUp")
-    public ResponseEntity<String> insertNewUser(User user){
+    public ResponseEntity<String> insertNewUser(@RequestBody User user){
         if(dao.Create(user))
             return new ResponseEntity<>("The user has created",HttpStatus.OK);
         else
