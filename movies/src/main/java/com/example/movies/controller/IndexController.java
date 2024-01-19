6d8abfd1c64876555dc09ec657a0085b500d11bb
ManosMorf97 <manosmorf97@gmail.com>
2024-01-19 13:05:04 +0200

    Thymeleaf

---
+++ movies/src/main/java/com/example/movies/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.movies.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/Sign_Up")
+    public String Sign_Up(){
+        return "Sign_Up";
+    }
+    @GetMapping("/")
+    public String Sign_In(){
+        return "Sign_In";
+    }
+    @GetMapping("/Welcome")
+    public String Welcome(){
+        return "Welcome";
+    }
+    @GetMapping("/BookMarks")
+    public String BookMarks(){
+        return "BookMarks";
+    }
+}
