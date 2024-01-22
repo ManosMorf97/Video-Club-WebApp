55d90a1606fb83458b5aa00502b8cbfc216344c8
ManosMorf97 <manosmorf97@gmail.com>
2024-01-22 18:19:06 +0200

    opfos

--- movies/src/main/resources/static/src/movie_operations.js
+++ movies/src/main/resources/static/src/movie_operations.js
@@ -1,5 +1,35 @@
 let spanners=document.getElementsByTagName("span");
 
+/*
+<div class="external-div">
+                <h1>Batman Begins</h1>
+                <h2>2005</h2>
+                <img src="img1.jpg" alt="none" width="200" height="200" />
+                <br>
+                <div class="bottom">
+                    <a href="" id="more" >more..</a>
+                </div>
+                <br>
+            </div>
+            <div class="external-div">
+                <h1>Batman Begins</h1>
+                <h2>2005</h2>
+                <img src="img1.jpg" alt="none" width="200" height="200" />
+                <br>
+                <h3>Movie</h3>
+                <h3>tt0372784</h3>
+                <button class="buttonS">LIKE</button>
+                <div class="bottom">
+                    <a href="" >less</a>
+                </div>
+                <br>
+            </div>
+
+
+
+
+*/
+
 function changeindexedcolor(i){
     return function changecontentcolor(e){
         console.log("WWWWWWWWWWW")
@@ -10,3 +40,11 @@ function changeindexedcolor(i){
 for(let i=0; i<spanners.length; i++){
     spanners[i].addEventListener("mouseover",changeindexedcolor(i));
 }
+
+document.getElementById("inputSearch").addEventListener("keyup",(e)=>{
+    let search_value=document.getElementById("inputSearch").value
+    let url="http://www.omdbapi.com/?apikey=1c07e2b7&&s="+search_value
+    fetch(url).then((response)=>{
+        console.log(response.body);
+    })
+})
